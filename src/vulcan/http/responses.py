b2a914c68703f28fb7a0da2a343c5993d3af2e82
jonathan.westcott <jonathan.westcott123@hotmail.co.uk>
2021-10-03 15:32:16 +0100

    Added dataclass for HTTP response messages

--- src/vulcan/http/responses.py
+++ src/vulcan/http/responses.py
@@ -12,4 +12,19 @@ class BaseResponse(TimestampMixin):
 
 
 class HTTPResponse(BaseResponse):
-    pass
+    """
+    Data class for keeping track of HTTP response information.
+    """
+
+    def __init__(self, http_version: str, status_code: int, phrase: str, headers:Dict[str, str], data: bytes):
+        super().__init__()
+        self.http_version = http_version
+        self.status_code = status_code
+        self.phrase = phrase
+        self.headers = headers
+        self.data = data
+
+    def __repr__(self) -> str:
+        return "HTTPResponse(http_version={}, status_code={}, phrase={})".format(self.http_version,
+                                                                                 self.status_code,
+                                                                                 self.phrase)
